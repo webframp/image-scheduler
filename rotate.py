f2cebc45d51b8d6a607d5b580ab7904da840cb7d
Sean Escriva <sean.escriva@gmail.com>
2012-10-22 14:50:40 -0700

    Bunch of minor refactoring based on cnelson review

--- rotate.py
+++ rotate.py
@@ -10,84 +10,85 @@
 #
 # Usage: Run this script on a scheduled basis (via cron) and then add the
 #   following to the startup script:"eval $(cat ~/.fehbg)"
-# 
+#
 # Depends:
 #   feh `apt-get -q -y install feh`
 #   correct mount point settings in /etc/fstab to allow user mounts (-o users)
 #
 #########
+
 import argparse
 import datetime
 import logging
 import os
-import errno
 import shutil
 import subprocess
 
-def image_index(img_key):
-    return {
-        'englishCA' : 'enCAimg',
-        'englishSA' : 'enSAimg',
-        'spanishCA' : 'esCAimg',
-        'spanishSA' : 'esSAimg',
-        }[img_key]
+image_index = {
+    'englishCA': 'enCAimg',
+    'englishSA': 'enSAimg',
+    'spanishCA': 'esCAimg',
+    'spanishSA': 'esSAimg',
+}
+
 
 def get_current_week():
     return "week" + str(datetime.date.isocalendar(datetime.date.today())[1])
 
+
 def get_user_image_path(img):
     return os.path.join(os.environ['HOME'], 'slides', img + '.jpg')
+
 
 def create_fehbg_file(img_path):
     # create the ~/.fehbg file for the user this script is run as
     feh_cmd = 'feh --bg-fill' + ' "' + get_user_image_path(img_path) + '"\n'
-    try: 
-        with open(os.path.expanduser('~/.fehbg'),'w') as f:
+    try:
+        with open(os.path.expanduser('~/.fehbg'), 'w') as f:
             f.write(feh_cmd)
-            f.close()
     except IOError as e:
-        logging.CRITICAL("ERROR: unable to write fehbg file with command: %s" % feh_cmd)
-        logging.debug("Error message was: %s" % e)
+        logging.exception("ERROR: unable to write fehbg file with command: %s",
+                          feh_cmd)
+        logging.debug("Error message was: %s", e)
+
 
 def create_local_dir(dir):
     # ensure local directory exists
-    try:
-        os.mkdir(os.path.join(os.environ['HOME'], dir))
-    except OSError as err:
-        if err.errno == errno.EEXIST:
-            pass
-        else:
-            logging.debug("An issue was encountered creating the local directory")
-            raise
+    local_dir = os.path.join(os.environ['HOME'], dir)
+    if os.path.exists(local_dir):
+        logging.warning("Directory already exists")
+        return
+    else:
+        os.makedirs(local_dir)
+        logging.debug("Directory created: %s", local_dir)
+
 
 def ensure_mount_point_exists(mountpoint):
     mount_args = ["mount", mountpoint]
-    try:
-        os.mkdir(mountpoint)
-        if subprocess.call(mount_args) != 0:
-            logging.debug("unable to ensure mounted filesystem: %s" % mountpoint)
-    except OSError as err:
-        if err.errno == errno.EEXIST:
-            pass
-        else:
-            logging.debug("An issue was encountered creating with the network share")
-            logging.debug("Error was: %s" % err)
-            raise
+    if os.path.exists(mountpoint):
+        logging.warning("Mount point already exists: %s Doing nothing",
+                        mountpoint)
+        return
+    else:
+        os.makedirs(mountpoint)
+        subprocess.call(mount_args) != 0
+
 
 def copy_images_from_share(src, dst):
     # refresh all images from image share (delete and recopy)
     try:
         shutil.rmtree(dst, ignore_errors=True)
         shutil.copytree(src, dst)
-    except OSError as err:
-        logging.CRITICAL("An error occured: %s" % err)
+    except OSError as e:
+        logging.critical("An error occured: %s", e)
+
 
 def main():
     # arguments are fun!
     parser = argparse.ArgumentParser(description='Utility script to parse SAD/CA schedule file and update theme images')
-    parser.add_argument('--debug', dest='verbose', action='store_const', const=1, help='Enable debug logging (default: off)')
+    parser.add_argument('--debug', dest='verbose', action='store_true', default=False, help='Enable debug logging (default: off)')
     parser.add_argument('--filename', dest='filename', action='store', default='schedule.txt', help='Filename of schedule file (default: schedule.txt)')
-    parser.add_argument('--logfile', dest='logfile', default=False, help='Log to a file instead of stdout (default: None)')
+    parser.add_argument('--logfile', dest='logfile', default=None, help='Log to a file instead of stdout (default: None)')
     args = parser.parse_args()
 
     # handle some args
@@ -100,8 +101,8 @@ def main():
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
-    
-    logging.debug("All args: %s" % args)
+
+    logging.debug("All args: %s", args)
     # process
     try:
         process_dir = os.path.expanduser('~/slides/')
@@ -109,19 +110,16 @@ def main():
             for line in f:
                 config = line.rstrip().split('=')
                 if config[0] == get_current_week():
-                    # determine value in config[]
-                    logging.debug("current week found in file as:  %s" % config[0])
-                    logging.debug("image key name: %s" % config[1])
-                    # and set file accordingly
-                    # link_file() or create_dot_fehbg_file()
-                    logging.debug("image file name: %s" % image_index(config[1]))
+                    logging.debug("current week found in file as:  %s", config[0])
+                    logging.debug("image key name: %s", config[1])
+                    logging.debug("image file name: %s", image_index[config[1]])
                     create_local_dir('slides')
-                    create_fehbg_file(image_index(config[1]))
-                    ensure_mount_point_exists("/mnt/wahdocs")
-                    copy_images_from_share("/mnt/wahdocs/slides", # FIX
-                                           os.path.expanduser("~/slides"))
+                    # ensure_mount_point_exists("/mnt/wahdocs")
+                    # copy_images_from_share("/mnt/wahdocs/slides", # FIX
+                    #                        os.path.expanduser("~/slides"))
+                    create_fehbg_file(image_index[config[1]])
     except IOError as e:
-        logging.CRITICAL("ERROR: schedule file not found: %s, %s" % args.filename, e)
+        logging.exception("ERROR: schedule file not found: %s, %s", args.filename, e)
 
 if __name__ == '__main__':
     main()
